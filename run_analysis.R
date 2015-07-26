be70bf1c724a5964ec42fc6d036e65aa10db1f88
Hank <hcheng10@gmail.com>
2015-07-25 21:49:26 -0700

    first major commit answer part 1 of the project

--- run_analysis.R
+++ run_analysis.R
@@ -3,3 +3,42 @@
 ## 3. Uses descriptive activity names to name the activities in the data set
 ## 4. Appropriately labels the data set with descriptive variable names. 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## create variables
+alabel <- read.table("./activity_labels.txt")
+feature <-read.table("./features.txt")
+xtest <- read.table("./test/X_test.txt")
+ytest <- read.table("./test/y_test.txt")
+subtest <- read.table("./test/subject_test.txt")
+names(xtest) <- feature$V2
+
+ytrain <- read.table("./train/y_train.txt")
+xtrain <- read.table("./train/X_train.txt")
+subtrain <- read.table("./train/subject_train.txt")
+names(xtrain) <- feature$V2
+
+## merge data
+## first merge in subject and activity data columns into data
+test <- cbind(subtest, ytest, xtest)
+colnames(test)[1] <- "Subject"
+colnames(test)[2] <- "Activity"
+
+train <- cbind(subtrain, ytrain, xtrain)
+colnames(train)[1] <- "Subject"
+colnames(train)[2] <- "Activity"
+## merge data from test and training data
+merged <- rbind(train, test)
+
+## extract names data from merged and determine if they include mean or std
+datname <- names(merged)
+meansd <- logical(length = length(datname))
+for (i in 1:length(datname)) {
+    if (i == 1 || i == 2) {
+        meansd[i] <- TRUE
+    } else {
+        meansd[i] <- grepl("mean|std",datname[i])
+    }
+}
+## subset out 
+
+
